d4166dc6aba6d3845d6a1f720e038ab9936ab494
Jessica Neves <128631606+jessicamdsn@users.noreply.github.com>
2025-03-02 16:20:07 -0300

    add footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { BodyComponent } from '../../components/body/body.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    FooterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
